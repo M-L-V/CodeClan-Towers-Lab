4026e59a0f9ba1bd5985f260f3883a14aa78f194
Morgaine <morgainedv@gmail.com>
2020-01-08 13:48:39 +0000

    adds set up for hotel test class

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Hotel {
+
+    private String hotelName;
+    private ArrayList<Bedroom> bedrooms;
+    private ArrayList<ConferenceRoom> conferenceRooms;
+
+    public Hotel (String hotelName, ArrayList<Bedroom> bedrooms, ArrayList<ConferenceRoom> conferenceRooms){
+        this.hotelName = hotelName;
+        this.bedrooms = new ArrayList<Bedroom>();
+        this.conferenceRooms = new ArrayList<ConferenceRoom>();
+    }
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -1,2 +1,36 @@
+import org.junit.Before;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class HotelTest {
+
+    private Hotel hotel;
+    private Bedroom bedroom1;
+    private Bedroom bedroom2;
+    private ConferenceRoom conferenceRoom1;
+    private ConferenceRoom conferenceRoom2;
+    private Guest guest1;
+    private Guest guest2;
+
+    @Before
+        public void before(){
+        bedroom1 = new Bedroom(101, 2, "double");
+        bedroom2 = new Bedroom(102, 1, "single");
+        conferenceRoom1 = new ConferenceRoom( 50, "Mike Ritchie");
+        conferenceRoom2 = new ConferenceRoom(60, "Grace Hopper");
+        guest1 = new Guest("Sandy");
+        guest2 = new Guest("Eugene");
+
+        ArrayList<Bedroom> bedroomsList = new ArrayList<Bedroom>();
+        bedroomsList.add(bedroom1);
+        bedroomsList.add(bedroom2);
+
+        ArrayList<ConferenceRoom> conferenceRoomsList = new ArrayList<ConferenceRoom>();
+        conferenceRoomsList.add(conferenceRoom1);
+        conferenceRoomsList.add(conferenceRoom2);
+
+        hotel = new Hotel ("CodeClan Towers", bedroomsList, conferenceRoomsList);
+    }
+
 }
