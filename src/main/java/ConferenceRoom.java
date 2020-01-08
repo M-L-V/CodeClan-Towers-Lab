6f30ab4d8a263fa11253513f76b187cf912d6d6d
Morgaine <morgainedv@gmail.com>
2020-01-08 13:19:57 +0000

    adds set up for conference room class

--- src/main/java/ConferenceRoom.java
+++ src/main/java/ConferenceRoom.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class ConferenceRoom {
+
+
+    private int capacity;
+    private String roomName;
+    private ArrayList<Guest> guests;
+
+
+    public ConferenceRoom (int capacity, String roomName){
+        this.capacity = capacity;
+        this.roomName = roomName;
+        this.guests = new ArrayList<Guest>();
 }
