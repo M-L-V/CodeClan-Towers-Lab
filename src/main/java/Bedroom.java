fd90a8a1f83d454b904d5a230b0b684f510f8113
Morgaine <morgainedv@gmail.com>
2020-01-08 11:57:16 +0000

    adds guest room number method

--- src/main/java/Bedroom.java
+++ src/main/java/Bedroom.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class Bedroom {
+
+    private int roomNumber;
+    private int capacity;
+    private String type;
+    private ArrayList<Guest> guests;
+
+    public Bedroom(int roomNumber, int capacity, String type){
+        this.roomNumber = roomNumber;
+        this.capacity = capacity;
+        this.type = type;
+        this.guests = new ArrayList<Guest>();
+    }
+
+    public int getRoomNumber() {
+        return this.roomNumber;
+    }
 }
--- src/test/java/BedroomTest.java
+++ src/test/java/BedroomTest.java
@@ -1,2 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BedroomTest {
+
+    private Bedroom bedroom1;
+    private Bedroom bedroom2;
+    private Guest guest1;
+    private Guest guest2;
+
+    @Before
+    public void before(){
+        bedroom1 = new Bedroom(101, 2, "double");
+        bedroom2 = new Bedroom(102, 1, "single");
+        guest1 = new Guest("Sandy");
+        guest2 = new Guest("Eugene");
+    }
+
+    @Test
+    public void canGetRoomNumber(){
+        assertEquals(101, bedroom1.getRoomNumber());
+    }
+
+
 }
